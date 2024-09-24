c0530192641cb29b0dd3d295716940b63457bc5e
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-24 11:56:43 +0530

    Bug fixes and project styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import ButtonGradient from "./assets/svg/ButtonGradient.tsx";
+import Button from "./components/Button.tsx";
 
 function App() {
-  return <h1 className="font-bold underline text-xl">Hello World</h1>;
+  return (
+    <>
+      <h1 className="font-bold underline text-xl">Hello World</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
