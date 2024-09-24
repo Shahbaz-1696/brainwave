c34ae5b9d3c4c6d918bf88c00af249548fce334e
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-24 20:55:38 +0530

    Completed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.tsx";
-import Button from "./components/Button.tsx";
+
+import Header from "./components/Header.tsx";
 
 function App() {
   return (
     <>
-      <h1 className="font-bold underline text-xl">Hello World</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login">
-          Something
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
