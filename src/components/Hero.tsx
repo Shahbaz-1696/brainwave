fe219d06ec953ec5ef9300a56391be1c29cb33a0
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-09-25 15:40:21 +0530

    Bug fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
-      crosses=""
+      crosses
       crossesOffset="lg:transparent-y-[5.25rem]"
       customPaddings=""
       id="hero"
